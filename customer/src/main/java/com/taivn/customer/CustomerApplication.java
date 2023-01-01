3ec02d7401fff5e3b046148bc99a475febdcecb5
Taffy <nguyenvantai22298@gmail.com>
2023-01-01 16:39:35 +0700

    Initialize cÃustomer module

---
+++ customer/src/main/java/com/taivn/customer/CustomerApplication.java
@@ -0,0 +1,15 @@
+package com.taivn.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tai VN
+ * @date 1/1/2023
+ */
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
