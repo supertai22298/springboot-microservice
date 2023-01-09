7324ea03a354a427a25ae005025cdffa97488b7e
Taffy <nguyenvantai22298@gmail.com>
2023-01-10 02:32:42 +0700

    Add spring resilience4j, actuator for handling circuit breaker

--- customer/src/main/java/com/taivn/customer/CustomerController.java
+++ customer/src/main/java/com/taivn/customer/CustomerController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * @author Tai VN
  * @date 1/1/2023
@@ -21,14 +23,15 @@ public class CustomerController {
     String title;
 
     @PostMapping
-    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
+    public String registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("START - New customer registration {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
         log.info("End - New customer registration\n");
+        return "Register successful";
     }
 
     @GetMapping
-    public String getTitle() {
-        return title;
+    public List<Customer> getTitle() {
+        return customerService.findAll();
     }
 }
--- customer/src/main/java/com/taivn/customer/CustomerService.java
+++ customer/src/main/java/com/taivn/customer/CustomerService.java
@@ -1,17 +1,32 @@
 package com.taivn.customer;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Tai VN
  * @date 1/1/2023
  */
 @Service
-public record CustomerService(
-        CustomerRepository customerRepository,
-        RestTemplate restTemplate,
-        FraudClient fraudClient) {
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    //    private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
+
+    public CustomerService(
+            CustomerRepository customerRepository,
+//                           RestTemplate restTemplate,
+            FraudClient fraudClient) {
+        this.customerRepository = customerRepository;
+//        this.restTemplate = restTemplate;
+        this.fraudClient = fraudClient;
+    }
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -37,5 +52,22 @@ public record CustomerService(
         if (response.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
+    }
+
+    @CircuitBreaker(name = "customerFindAllCB", fallbackMethod = "findAllFallback")
+    public List<Customer> findAll() {
+        FraudCheckResponse response = fraudClient.checkFraud(2);
+        return customerRepository.findAll();
+    }
+
+    /**
+     * This fallback is call when findAll function has error
+     * @param e
+     * @return
+     */
+    public List<Customer> findAllFallback(Exception e) {
+        List<Customer> list = new ArrayList<>();
+        list.add(Customer.builder().firstName("Something went wrong").build());
+        return list;
     }
 }
